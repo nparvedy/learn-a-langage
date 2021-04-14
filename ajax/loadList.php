170af5d33332ef456f02c264a9baf9a34944e689
nparvedy <nicolasparvedy@gmail.com>
2021-04-14 10:50:52 +0200

    update for select a word

--- ajax/loadList.php
+++ ajax/loadList.php
@@ -4,6 +4,8 @@ require '../class/BDD.php';
 $bdd = new BDD;
 
 $list_word = $bdd->getWordList($_POST['list']);
+$test = str_replace("'", '&#39;', json_encode($list_word));
+$test = str_replace('"', '&quot;', $test);
 
 ?>
 
@@ -12,6 +14,6 @@ $list_word = $bdd->getWordList($_POST['list']);
         <p>Vous avez bien chargé votre liste</p>
         <p>Pour démarrer le test, cliquer sur commencer</p>
 
-        <button type="button" class="btn btn-primary" onclick='quizz.startQuizz(<?php echo json_encode($list_word); ?>)'>Commencer</button>
+        <button type="button" class="btn btn-primary" onclick="quizz.startQuizz(<?php echo $test ?>)">Commencer</button>
     </div>
 </div>
--- sources/quizz.js
+++ sources/quizz.js
@@ -8,6 +8,10 @@ class Quizz {
     resultCheck = "";
     rightCounter = 0;
     wrongCounter = 0;
+    prioritizeWordBoard = [];
+    reverse = false;
+
+    prioritizeWordBoardNew = [];
 
     static HARDCORE = 20;
     static REALLY_HARD = 40;
@@ -31,13 +35,13 @@ class Quizz {
             this.checkWord(this.mytranslate.value);
 
         }
-        this.wordActual = this.selectWord();
+        this.wordActual = this.selectWordNew();
 
         this.content.innerHTML = `
         <div class="row align-items-md-stretch">
             <div class="col-md-6">
                 <div class="h-100 p-4  border rounded-3">
-                    <p>Le mot à traduire est :  ${this.listWord[this.wordActual]['word_to_translate']}</p>
+                    <p>Le mot à traduire est :  ${(this.reverse ? this.listWord[this.wordActual]['word_translated'] : this.listWord[this.wordActual]['word_to_translate'])}</p>
                     <div class="row g-3 last">
                         <div class="mb-3 col-sm-6 ">
                             <label for="" class="form-label">Traduction</label>
@@ -54,11 +58,11 @@ class Quizz {
         </div>
         <div class="d-grid gap-2 mt-3 d-sm-flex">
             <button type="button" class="btn btn-primary"  onclick="quizz.next()">Suivant</button>
-            <button type="button" class="btn btn-primary" onclick="quizz.stop()">Stop</button>
+            <button type="button" class="btn btn-danger" onclick="quizz.stop()">Stop</button>
+            <button type="button" class="btn btn-info" onclick="quizz.reverseMethod()">Reverse</button>
         </div>`;
 
         this.mytranslate = document.querySelector("#myTranslate");
-
         this.event();
         this.mytranslate.focus();
 
@@ -67,6 +71,7 @@ class Quizz {
         } else {
             this.result();
         }
+
 
     }
 
@@ -96,19 +101,42 @@ class Quizz {
         xhr.send(data);
     }
 
+    reverseMethod() {
+        if (this.reverse) {
+            this.reverse = false;
+        } else {
+            this.reverse = true;
+        }
+        this.mytranslate = null;
+        this.next();
+    }
+
+    //version 2 de selectWord
+    selectWordNew() {
+
+        if (this.prioritizeWordBoardNew.length == 5) {
+            const random = Math.floor(Math.random() * this.prioritizeWordBoardNew.length);
+
+            return this.prioritizeWordBoardNew[random];
+        } else {
+            return this.selectWord();
+        }
+    }
+
+    //obsolète version
     selectWord() {
         var arrayWord = [];
         for (var i = 0; i < this.listWord.length; i++) {
             this.calculSuccessRate(i);
             if (parseInt(this.listWord[i]['success_rate']) <= Quizz.HARDCORE) {
                 var a = 0;
-                while (a < 10) {
+                while (a < Math.round(this.listWord.length)) {
                     arrayWord.push(i);
                     a++;
                 }
             } else if (parseInt(this.listWord[i]['success_rate']) <= Quizz.REALLY_HARD) {
                 var a = 0;
-                while (a < 6) {
+                while (a < Math.round(this.listWord.length / 2)) {
                     arrayWord.push(i);
                     a++;
                 }
@@ -130,21 +158,87 @@ class Quizz {
 
         }
 
+        //si on se trompe trop sur un mot alors il apparait beaucoup plus souvent jusqu'à y arriver
+        //ce qui évite que trop de mot hardcore apparaisse de manière régulière
+        //ou qu'on ne tombe pas assez sur des mots dont on ne maitrise pas totalement
+        if (this.prioritizeWordBoard.length != 0) {
+            var a = 0;
+            while (a < this.listWord.length * this.prioritizeWordBoard[1]) {
+                arrayWord.push(this.prioritizeWordBoard[0]);
+                a++;
+            }
+        }
+
         arrayWord = arrayWord.sort(() => Math.random() - 0.5);
 
         const random = Math.floor(Math.random() * arrayWord.length);
+        //return number
         return arrayWord[random];
     }
 
+    addPrioritizeWord(idWord) {
+        if (this.prioritizeWordBoard.length == 0) {
+
+            this.prioritizeWordBoard = [idWord, 1];
+
+        } else if (this.prioritizeWordBoard[0] == idWord) {
+
+            this.prioritizeWordBoard[1] = 2;
+
+        }
+    }
+
+    addPrioritizeWordNew(idWord) {
+        if (this.prioritizeWordBoard.length == 0) {
+            this.prioritizeWordBoardNew = [idWord];
+        } else if (this.prioritizeWordBoardNew.length < 5) {
+            if (!this.prioritizeWordBoardNew.includes(idWord)) {
+                this.prioritizeWordBoardNew.push(idWord);
+            }
+        }
+
+    }
+
+    deleteWordPrioritizeNew(idWord) {
+        if (this.prioritizeWordBoardNew.includes(idWord)) {
+
+            if (this.listWord[idWord]['success_rate'] > Quizz.HARD) {
+                const index = this.prioritizeWordBoardNew.indexOf(idWord);
+
+                if (index > - 1) {
+                    this.prioritizeWordBoardNew.splice(index, 1);
+                }
+            }
+        }
+    }
+
     checkWord(WordTranslated) {
-        if (this.listWord[this.wordActual]['word_translated'].toLowerCase() == WordTranslated.toLowerCase()) {
+
+        if ((this.reverse ? this.listWord[this.wordActual]['word_to_translate'].toLowerCase().trim() : this.listWord[this.wordActual]['word_translated'].toLowerCase().trim()) == WordTranslated.toLowerCase().trim()) {
             this.listWord[this.wordActual]['right_counter']++;
             this.rightCounter++;
             this.resultCheck = "good";
+            this.calculSuccessRate(this.wordActual);
+
+            //Si on tombe sur le bon mot alors on réinitialise  
+            if (this.prioritizeWordBoardNew != 0) {
+                this.deleteWordPrioritizeNew(this.wordActual);
+            }
+
+            //Si on tombe sur le bon mot alors on réinitialise 
+            if (this.prioritizeWordBoard != 0) {
+                if (this.prioritizeWordBoard[0] == this.wordActual) {
+                    this.prioritizeWordBoard = [];
+                }
+            }
         } else {
             this.listWord[this.wordActual]['wrong_counter']++;
             this.wrongCounter++;
             this.resultCheck = "wrong";
+            this.calculSuccessRate(this.wordActual);
+
+            this.addPrioritizeWord(this.wordActual);
+            this.addPrioritizeWordNew(this.wordActual);
         }
     }
 
@@ -166,7 +260,7 @@ class Quizz {
             resultObject.innerHTML = `
                 <h2>Résultat</h2>
                 <div class="alert alert-success" role="alert">
-                    Bravo le bon mot était bien : ${this.listWord[this.lastWord]['word_translated']}
+                    Bravo le bon mot était bien : ${(this.reverse ? this.listWord[this.lastWord]['word_to_translate'] : this.listWord[this.lastWord]['word_translated'])}
                 </div>
                 <p>Vous avez ${this.rightCounter} bon et ${this.wrongCounter} faux.</p>
                 `;
@@ -174,7 +268,7 @@ class Quizz {
             resultObject.innerHTML = `
             <h2>Résultat</h2>
             <div class="alert alert-danger" role="alert">
-                Et non ! La traduction était plutôt : ${this.listWord[this.lastWord]['word_translated']}
+                Et non ! La traduction était plutôt : ${(this.reverse ? this.listWord[this.lastWord]['word_to_translate'] : this.listWord[this.lastWord]['word_translated'])}
             </div>
             <p>Vous avez ${this.rightCounter} bon et ${this.wrongCounter} faux.</p>
             `
@@ -189,3 +283,6 @@ function next() {
     quizz.next();
 }
 
+// function reverse() {
+//     quizz.reverse();
+// }
